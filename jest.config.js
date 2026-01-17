18c5bd04fb37ceb23caefd164fc381ca94a52eb3
Fernando Mirabile <fernando.mirabileE@jampp.com>
2026-01-17 18:02:46 +0000

    More setup configuration. Trying to make p-limit work with ESM and not use extensions in ts files.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
-module.exports = {
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
   testEnvironment: "node",
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
-    ...tsJestTransformCfg,
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
 };
