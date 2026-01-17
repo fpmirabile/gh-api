c5068778f37fd09daebe3c00a2e2e3751b7c11cb
Fernando Mirabile <fernando.mirabileE@jampp.com>
2026-01-17 18:19:41 +0000

    Removing non commonjs code.

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,6 @@ import healthRoutes from './routes/health.routes';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
