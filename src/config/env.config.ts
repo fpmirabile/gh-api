612b4ab7816a16b9895cf29c6ba7775be3fad2db
Fernando Mirabile <fernando.mirabileE@jampp.com>
2026-01-17 18:05:41 +0000

    Adding config interface.

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,7 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const config = {
+export interface Config {
+  port: string | number;
+  githubToken: string | undefined;
+  githubBaseUrl: string;
+}
+
+export const config: Config = {
   port: process.env.PORT || 3000,
   githubToken: process.env.GITHUB_TOKEN,
   githubBaseUrl: 'https://api.github.com'
